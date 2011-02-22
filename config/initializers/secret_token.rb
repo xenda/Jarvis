f7653337b3e11db352fb13c1a25572d5a9afa0f4
HPNeo <hpneo@hotmail.com>
2011-02-22 14:54:27 -0500

    Inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jarvis::Application.config.secret_token = '9a5902ebf2efd2bf72e52b0999b7b81294f876f0dbc56a2eb662901bc5b7b161953fda060cb0d2fb43716625b8d384a21b3f842d6d8db7c1fbdc4eaf5a98fab4'
