af3351625f0cf2856eb078ef6106103c066387af
HPNeo <hpneo@hotmail.com>
2011-02-23 17:37:45 -0500

    Mejorada la estructura HTML de la app y CSS agregado.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveResource::Base
 	
 	self.site = "http://www.pivotaltracker.com/services/v3"
-	headers['X-TrackerToken'] = '3fa226bc048021e8467040561f403381'
+	self.headers['X-TrackerToken'] = '3fa226bc048021e8467040561f403381'
 	
 	def messages
 		Message.where(:project_id => self.id)
